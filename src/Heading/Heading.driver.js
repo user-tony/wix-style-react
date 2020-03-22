8a3acdd294f8956e98e73b0c6e377939d599a359
Mykolas Sindeikis <mykolass@wix.com>
2020-03-22 11:26:36 +0200

    <Heading/> - migrating regular driver to not use stylable stylesheet for testing (#5230)

--- src/Heading/Heading.driver.js
+++ src/Heading/Heading.driver.js
@@ -1,15 +1,11 @@
-import { StylableDOMUtil } from '@stylable/dom-test-kit';
-import style from './Heading.st.css';
-
 const headingDriverFactory = factoryParams => {
-  const stylableDOMUtil = new StylableDOMUtil(style);
   const { element } = factoryParams;
 
   return {
     exists: () => !!element,
     getText: () => element.innerHTML,
-    getAppearance: () => stylableDOMUtil.getStyleState(element, 'appearance'),
-    isLight: () => stylableDOMUtil.hasStyleState(element, 'light'),
+    getAppearance: () => element.getAttribute('data-appearance'),
+    isLight: () => element.getAttribute('data-light') === 'true',
   };
 };
 
