b5c7fcbb9513329e22aa3c1c8d9941c223215dc5
Mykolas Sindeikis <mykolass@wix.com>
2020-03-22 11:54:22 +0200

    `<Text/>` - migrate regular driver to not use stylesheet for testing (#5231)
    * <Text/> - migrate regular driver to not use stylesheet for testing
    
    * Revert usage of generateAttr to check if that causes the bundle size increase

--- src/Text/Text.driver.js
+++ src/Text/Text.driver.js
@@ -1,17 +1,13 @@
-import { StylableDOMUtil } from '@stylable/dom-test-kit';
-import style from './Text.st.css';
-
 const textDriverFactory = ({ element }) => {
-  const stylableDOMUtil = new StylableDOMUtil(style);
   return {
     exists: () => !!element,
     getTagName: () => element.tagName.toLowerCase(),
     getText: () => element.innerHTML,
-    getSize: () => stylableDOMUtil.getStyleState(element, 'size'),
-    getSkin: () => stylableDOMUtil.getStyleState(element, 'skin'),
-    getWeight: () => stylableDOMUtil.getStyleState(element, 'weight'),
-    isLight: () => stylableDOMUtil.hasStyleState(element, 'light'),
-    isSecondary: () => stylableDOMUtil.hasStyleState(element, 'secondary'),
+    getSize: () => element.getAttribute('data-size'),
+    getSkin: () => element.getAttribute('data-skin'),
+    getWeight: () => element.getAttribute('data-weight'),
+    isLight: () => element.getAttribute('data-light') === 'true',
+    isSecondary: () => element.getAttribute('data-secondary') === 'true',
   };
 };
 
