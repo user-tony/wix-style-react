74e490f3851fb817deaa711cafe363443b23b751
Egozi <egozi@wix.com>
2020-03-27 17:07:02 +0300

    <Card.Subheader/> - fix story page

--- src/Card/Subheader/docs/index.story.js
+++ src/Card/Subheader/docs/index.story.js
@@ -6,6 +6,8 @@ import {
   importExample,
   testkit,
   playground,
+  header,
+  tabs,
 } from 'wix-storybook-utils/Sections';
 import allComponents from '../../../../stories/utils/allComponents';
 import * as examples from './examples';
@@ -25,47 +27,55 @@ export default {
   },
 
   sections: [
-    tab({
-      title: 'Usage',
-      sections: [
-        importExample({
-          source: `
+    header({
+      issueUrl: 'https://github.com/wix/wix-style-react/issues/new',
+      sourceUrl:
+        'https://github.com/wix/wix-style-react/tree/master/src/Card/Subheader/',
+    }),
+
+    tabs([
+      tab({
+        title: 'Usage',
+        sections: [
+          importExample({
+            source: `
             import Card from 'wix-style-react/Card';
 const Subheader = Card.Subheader;
             `,
-        }),
-        code({
-          title: 'Example usage',
-          compact: true,
-          source: examples.sampleUsage,
-        }),
-        code({
-          title: 'With text title prop',
-          compact: true,
-          source: examples.textTitle,
-        }),
-        code({
-          title: 'Neutral skin',
-          compact: true,
-          source: examples.neutralSkin,
-        }),
-        code({
-          title: 'With custom node as title',
-          compact: true,
-          source: examples.nodeTitle,
-        }),
-        code({
-          title: 'With custom suffix',
-          compact: true,
-          source: examples.suffix,
-        }),
-      ],
-    }),
+          }),
+          code({
+            title: 'Example usage',
+            compact: true,
+            source: examples.sampleUsage,
+          }),
+          code({
+            title: 'With text title prop',
+            compact: true,
+            source: examples.textTitle,
+          }),
+          code({
+            title: 'Neutral skin',
+            compact: true,
+            source: examples.neutralSkin,
+          }),
+          code({
+            title: 'With custom node as title',
+            compact: true,
+            source: examples.nodeTitle,
+          }),
+          code({
+            title: 'With custom suffix',
+            compact: true,
+            source: examples.suffix,
+          }),
+        ],
+      }),
 
-    ...[
-      { title: 'API', sections: [api()] },
-      { title: 'TestKit', sections: [testkit()] },
-      { title: 'Playground', sections: [playground()] },
-    ].map(tab),
+      ...[
+        { title: 'API', sections: [api()] },
+        { title: 'TestKit', sections: [testkit()] },
+        { title: 'Playground', sections: [playground()] },
+      ].map(tab),
+    ]),
   ],
 };
