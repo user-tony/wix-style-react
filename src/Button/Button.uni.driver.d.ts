448ec6ec19b37c92564a5383ebacd42408f70c90
dorduch <dord@wix.com>
2020-04-13 14:45:35 +0300

    fixed Button.uni.driver.d.ts `hasSkin` argument type (#5341)

--- src/Button/Button.uni.driver.d.ts
+++ src/Button/Button.uni.driver.d.ts
@@ -1,9 +1,9 @@
 import { BaseUniDriver } from 'wix-ui-test-utils/base-driver';
-import { ButtonSize } from './index';
+import {ButtonSkin} from './index';
 
 export interface ButtonUniDriver extends BaseUniDriver {
   getButtonTextContent: () => Promise<string>;
   isButtonDisabled: () => Promise<boolean>;
   isFocused: () => Promise<boolean>;
-  hasSkin: (skinName: ButtonSize) => Promise<boolean>;
+  hasSkin: (skinName: ButtonSkin) => Promise<boolean>;
 }
