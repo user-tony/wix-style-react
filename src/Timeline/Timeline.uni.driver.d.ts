7053d114eea056f6115a4c6da9892aaaf215351b
Adi Mor <47712745+adimorwix@users.noreply.github.com>
2020-04-30 13:14:15 +0300

    <Timeline/> - fix driver types - missing index (#5422)

--- src/Timeline/Timeline.uni.driver.d.ts
+++ src/Timeline/Timeline.uni.driver.d.ts
@@ -1,6 +1,6 @@
 import { BaseUniDriver } from 'wix-ui-test-utils/unidriver';
 
 export interface TimelineUniDriver extends BaseUniDriver {
-  getLabelText(): Promise<string>;
-  getSuffixText(): Promise<string>;
+  getLabelText(idx: number): Promise<string>;
+  getSuffixText(idx: number): Promise<string>;
 }
