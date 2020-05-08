77ba83e237b5bae6c37ea7887ae793899bdb19d6
argshook <argshook@gmail.com>
2020-05-08 15:04:58 +0300

    chore(*): stylable migration

--- src/ColorPicker/ColorPickerConverterHsb.js
+++ src/ColorPicker/ColorPickerConverterHsb.js
@@ -24,7 +24,11 @@ export default class ColorPickerConverterHsb extends React.PureComponent {
   render() {
     const { dataHook } = this.props;
     return (
+<<<<<<< HEAD
       <div className={st(classes.root)} data-hook={dataHook}>
+=======
+      <div className={st(classes.root)}>
+>>>>>>> 2ef14b682... chore(*): stylable migration
         <div className={classes.distribute}>
           <Input
             size="small"
