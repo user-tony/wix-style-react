2c70c580463214df9273d74129c74bb040e1b445
argshook <argshook@gmail.com>
2020-05-20 09:50:14 +0300

    chore(*): stylable migration

---
+++ src/FloatingHelper/ClosablePopover/index.d.ts
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { ClosablePopover } from './ClosablePopover';
+
+export default class FloatingHelper extends React.PureComponent<
+  ClosablePopoverProps
+> {
+  open: () => void;
+  close: () => void;
+}
+
+export interface ClosablePopoverProps {
+  opened?: boolean;
+  initiallyOpened?: boolean;
+  target: node.isRequired;
+  onOpen?: Function;
+  onClose?: Function;
+  closeOnMouseLeave?: boolean;
+  content: Function;
+  className?: string;
+}
