f4bd7608484c7c25ee6ef585bfc88e7cc65f4686
Tal Zaidman <talza@wix.com>
2020-05-24 13:30:00 +0300

    driver fix (#5491)

--- src/Notification/Notification.driver.js
+++ src/Notification/Notification.driver.js
@@ -5,35 +5,36 @@ const notificationDriverFactory = ({ element }) => {
   const getElementByDataHook = dataHook =>
     element.querySelector(`[data-hook="${dataHook}"]`);
 
-  const notificationWrapper = getElementByDataHook(
-    dataHooks.notificationWrapper,
-  );
-  const labelText = getElementByDataHook(dataHooks.notificationLabel);
-  const actionButton = getElementByDataHook(dataHooks.notificationCtaButton);
-  const closeButton = getElementByDataHook(dataHooks.notificationCloseButton);
+  const notificationWrapper = () =>
+    getElementByDataHook(dataHooks.notificationWrapper);
+  const labelText = () => getElementByDataHook(dataHooks.notificationLabel);
+  const actionButton = () =>
+    getElementByDataHook(dataHooks.notificationCtaButton);
+  const closeButton = () =>
+    getElementByDataHook(dataHooks.notificationCloseButton);
 
   const getTheme = () => element.getAttribute('data-theme');
   const getType = () => element.getAttribute('data-type');
 
   return {
     exists: () => !!element,
-    visible: () => !!getElementByDataHook(dataHooks.notificationWrapper),
+    visible: () => !!notificationWrapper(),
     hasTheme: () => !!getTheme(),
     isStandardNotification: () => getTheme() === THEMES.standard,
     isErrorNotification: () => getTheme() === THEMES.error,
     isSuccessNotification: () => getTheme() === THEMES.success,
     isWarningNotification: () => getTheme() === THEMES.warning,
     isPremiumNotification: () => getTheme() === THEMES.premium,
-    getLabelText: () => labelText.textContent,
-    hasActionButton: () => !!actionButton,
-    getActionButtonText: () => actionButton.textContent,
-    hasCloseButton: () => !!closeButton,
+    getLabelText: () => labelText().textContent,
+    hasActionButton: () => !!actionButton(),
+    getActionButtonText: () => actionButton().textContent,
+    hasCloseButton: () => !!closeButton(),
     isRelativelyPositioned: () => getType() === TYPE_POSITIONS_MAP.relative,
     isFixedPositioned: () => getType() === TYPE_POSITIONS_MAP.fixed,
     isAbsolutePositioned: () => getType() === TYPE_POSITIONS_MAP.absolute,
-    clickOnCloseButton: () => ReactTestUtils.Simulate.click(closeButton),
-    clickOnActionButton: () => ReactTestUtils.Simulate.click(actionButton),
-    getZIndex: () => Number(notificationWrapper.style['z-index']),
+    clickOnCloseButton: () => ReactTestUtils.Simulate.click(closeButton()),
+    clickOnActionButton: () => ReactTestUtils.Simulate.click(actionButton()),
+    getZIndex: () => Number(notificationWrapper().style['z-index']),
   };
 };
 
