5a134fa24f3be3d04e4fb95717a1e0ddcf585cb0
lironcoh <lironcoh@wix.com>
2020-05-25 21:38:57 +0300

    Fixing imports & types checks

--- src/BaseModalLayout/LayoutBlocks/Footnote/Footnote.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-interface FootnoteProps {
-  footnote?: React.ReactNode;
-}
-
-export const Footnote: React.FunctionComponent<FootnoteProps>;
--- src/BaseModalLayout/LayoutBlocks/Illustration/Illustration.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-
-interface IllustrationProps {
-  illustration?: string | React.ReactNode;
-  illustrationSize?: 'small' | 'large';
-}
-
-export const Illustration: React.FunctionComponent<IllustrationProps>;
