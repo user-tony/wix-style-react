44671aa8e9c85676a891ca733e922a291c78d60e
argshook <argshook@gmail.com>
2020-05-27 17:03:37 +0300

    chore(*.st.css): use -st-default for HOC stylable imports

--- scripts/transpile/tasks/replace-stylable-imports.js
+++ scripts/transpile/tasks/replace-stylable-imports.js
@@ -27,12 +27,13 @@ const PATHS_TO_CHANGE = [
 ];
 
 const WRONG_PATHS = [
+  /** temp ignore */
+  /**
   {
     regexp: /wix-ui-core\/dist\/src\/hocs\/.*\/[A-Za-z]*\.st\.css/,
     correct: 'hocs.st.css',
   },
-  /** temp ignore */
-  /** {
+  {
     regexp: /wix-ui-core\/dist\/src\/components\/.*\/[A-Za-z]*\.st\.css/,
     correct: 'index.st.css',
   }, */
