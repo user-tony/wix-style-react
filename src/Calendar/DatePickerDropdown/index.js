ddc416509ac81bbe693197cb2668ea6bb82a16b5
Egozi <egozi@wix.com>
2020-06-15 15:27:34 +0300

    <Calendar/> - Use DropdownBase internally

--- src/Calendar/DatePickerDropdown/index.js
+++ src/Calendar/DatePickerDropdown/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ChevronDown from 'wix-ui-icons-common/ChevronDown';
-import styles from './styles.scss';
 import DropdownBase from '../../DropdownBase';
 import TextButton from '../../TextButton';
+import Box from '../../Box';
 
 export default class DropdownPicker extends React.Component {
   static propTypes = {
@@ -24,29 +24,30 @@ export default class DropdownPicker extends React.Component {
     const { caption, options, dataHook, selectedId } = this.props;
 
     return (
-      <DropdownBase
-        data-hook={dataHook}
-        className={styles.root}
-        options={options}
-        dynamicWidth
-        minWidth={120}
-        selectedId={selectedId}
-        onSelect={this.onSelect}
-        focusOnSelectedOption
-      >
-        {({ toggle }) => {
-          return (
-            <TextButton
-              skin="dark"
-              suffixIcon={<ChevronDown />}
-              onClick={toggle}
-              dataHook={`${dataHook}-button`}
-            >
-              {caption}
-            </TextButton>
-          );
-        }}
-      </DropdownBase>
+      <Box padding="0 6px">
+        <DropdownBase
+          data-hook={dataHook}
+          options={options}
+          dynamicWidth
+          minWidth={120}
+          selectedId={selectedId}
+          onSelect={this.onSelect}
+          focusOnSelectedOption
+        >
+          {({ toggle }) => {
+            return (
+              <TextButton
+                skin="dark"
+                suffixIcon={<ChevronDown />}
+                onClick={toggle}
+                dataHook={`${dataHook}-button`}
+              >
+                {caption}
+              </TextButton>
+            );
+          }}
+        </DropdownBase>
+      </Box>
     );
   }
 }
