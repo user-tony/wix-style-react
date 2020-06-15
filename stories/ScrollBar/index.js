e5adef890bfee1845660da86b72203bc48a19332
TalZa <24567980+TalZa1810@users.noreply.github.com>
2020-06-15 11:31:48 +0300

    adding styling

--- stories/ScrollBar/index.js
+++ stories/ScrollBar/index.js
@@ -1,16 +1,42 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import Markdown from 'wix-storybook-utils/Markdown';
-
-import Readme from './Readme.md';
-import ExampleStandard from './ExampleStandard';
+import Button from '../../src/Button';
+import style from './ScrollBar.scss';
 
 import { Category } from '../storiesHierarchy';
 
-storiesOf(Category.FOUNDATION, module).add('1.8 ScrollBar', () => (
-  <div className="markdown-body">
-    <Markdown source={Readme} />
-    <h2>Example of scroll mixin usage</h2>
-    <ExampleStandard />
-  </div>
-));
+import { CSSTransition } from 'react-transition-group';
+
+storiesOf(Category.FOUNDATION, module).add('1.8 ScrollBar', () => {
+  const [show, setShow] = React.useState(false);
+
+  return (
+    <div>
+      <TempNotification show={show} />
+      text text text text text text text text text text text text
+      <Button className={style.button} onClick={() => setShow(!show)}>
+        Toggle
+      </Button>
+    </div>
+  );
+});
+
+const NotificationBox = () => (
+  <CSSTransition
+    classNames={{
+      enter: style.enter,
+      enterActive: style.enterActive,
+      exit: style.exit,
+      exitActive: style.exitActive,
+    }}
+  >
+    <div className={style.notification}></div>
+  </CSSTransition>
+);
+
+class TempNotification extends React.Component {
+  render() {
+    const { show } = this.props;
+    return show && <NotificationBox />;
+  }
+}
