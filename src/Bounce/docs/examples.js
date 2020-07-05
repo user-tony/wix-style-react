23a26e51a696a74045ed0b77b21701379d1fc915
TalZa <24567980+TalZa1810@users.noreply.github.com>
2020-07-05 16:48:10 +0300

    initial bounce

--- src/Bounce/docs/examples.js
+++ src/Bounce/docs/examples.js
@@ -1,20 +1,20 @@
 export const bounceExample = `() => {
-    const [ play, setPlay ] = React.useState(false)
+    const [ trigger, setTrigger ] = React.useState(false)
     return (
         <Layout>
-            <Cell span="4"><Button onClick={() => setPlay(true) }>Play</Button></Cell>
-            <Cell span="8">
-                <Bounce
-                  onEnter={()=> setPlay(true)}
-                  onEnter={()=> setPlay(false)}
-                  triggerAnimation={play}
-                >
-                    <FormField label="Field label">
-                        <Input size="small" placeholder="Placeholder" />
-                    </FormField>
-                </Bounce>
-              </Cell>
-            </Layout>
-      )
+          <Cell span='8'>
+            <Bounce
+              onEnter={() => setTrigger(true)}
+              onEnter={() => setTrigger(false)}
+              triggerAnimation={trigger}
+            >
+              <FormField label="Field label">
+                <Input size="small" placeholder="Placeholder" />
+              </FormField>
+            </Bounce>
+          </Cell>
+          <Cell><Button onClick={() => setTrigger(true)}>Play</Button></Cell>
+    </Layout>
+   )
 }
 `;
