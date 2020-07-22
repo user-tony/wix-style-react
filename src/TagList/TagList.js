b7f7a09a494d1963efb0e440947a2d69256a35cb
gendeld <danielge@wix.com>
2020-07-22 17:59:39 +0300

    Pass size prop to Tag and TagListAction

--- src/TagList/TagList.js
+++ src/TagList/TagList.js
@@ -5,46 +5,64 @@ import styles from './TagList.st.css';
 import Tag from '../Tag';
 import classNames from 'classnames';
 
+const tagToActionButtonSize = {
+  small: 'tiny',
+  medium: 'small',
+  large: 'medium',
+};
+
 /** TagList */
-const TagList = ({ dataHook, tags, actionButton }) => {
+const TagList = ({
+  dataHook,
+  tags,
+  actionButton,
+  size = 'small',
+  onTagRemove,
+}) => {
+  const actionButtonSize = tagToActionButtonSize[size];
+
   return (
     <div className={styles.root} data-hook={dataHook}>
       {tags.map(({ className, ...tagProps }) => (
         <Tag
           {...tagProps}
           className={classNames(styles.item, className)}
-          size="small"
+          size={size}
+          onRemove={onTagRemove}
           key={tagProps.id}
         />
       ))}
       {actionButton && (
         <TagListAction
+          {...actionButton}
           dataHook="tag-list-action"
-          onClick={actionButton.onClick}
-        >
-          {actionButton.label}
-        </TagListAction>
+          size={actionButtonSize}
+        />
       )}
     </div>
   );
 };
 
-const TagListAction = ({ dataHook, className, onClick, children, ...rest }) => (
+const TagListAction = ({
+  skin = 'inverted',
+  size = 'tiny',
+  className,
+  label,
+  ...buttonProps
+}) => (
   <Button
-    skin="inverted"
-    size="tiny"
+    skin={skin}
+    size={size}
     className={classNames(styles.item, className)}
-    dataHook={dataHook}
-    onClick={onClick}
-    {...rest}
+    {...buttonProps}
   >
-    {children}
+    {label}
   </Button>
 );
 
 TagListAction.propTypes = {
   dataHook: PropTypes.string,
-  children: PropTypes.node,
+  label: PropTypes.string.isRequired,
   onClick: PropTypes.func,
 };
 
@@ -55,7 +73,13 @@ TagList.propTypes = {
   dataHook: PropTypes.string,
 
   /** List of tags props to be rendered */
-  tags: PropTypes.arrayOf(PropTypes.object),
+  tags: PropTypes.arrayOf(PropTypes.object).isRequired,
+
+  /** The size of each individual `<Tag />` */
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+
+  /** Callback function that passes `id` property as parameter when removing a Tag  */
+  onTagRemove: PropTypes.func,
 
   /** Action button label and onClick handler */
   actionButton: PropTypes.shape({
