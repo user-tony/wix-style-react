75db3ce37602b6d7c2f1f2afeb0cda8511550275
sivanharel <sivanh@wix.com>
2020-07-28 16:32:34 +0300

    add support for rtl as well + implement the visual tests

--- src/MarketingPageTestimonialsFooter/test/MarketingPageTestimonialsFooter.visual.js
+++ src/MarketingPageTestimonialsFooter/test/MarketingPageTestimonialsFooter.visual.js
@@ -1,21 +1,75 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import MarketingPageTestimonialsFooter from '../MarketingPageTestimonialsFooter';
+import { RTLWrapper } from '../../../stories/utils/RTLWrapper';
+import Avatar from '../../Avatar';
 
 const commonProps = {
-  // use for repeated props across the tests (e.g. {buttonText: 'example'})
+  testimonials: [
+    {
+      id: '0001',
+      avatar: <Avatar name="Guy in glasses" size="size60" />,
+      text: 'I love it! This product is exactly what I needed.',
+      authorName: 'Guy in glasses',
+    },
+    {
+      id: '0002',
+      avatar: <Avatar name="Person with a hat" size="size60" />,
+      text: 'Amazing! It helped me to solve my problems.',
+      authorName: 'Person with a hat',
+    },
+    {
+      id: '0003',
+      avatar: <Avatar name="Smiling lady" size="size60" />,
+      text: 'A perfect tool for my every day tasks.',
+      authorName: 'Smiling lady',
+    },
+  ],
 };
 
 const tests = [
   {
-    describe: 'sanity', // prop name (e.g. size)
+    describe: 'sanity',
     its: [
-      {
-        it: 'default', // prop variation (e.g. small)
-        props: {
-          // the simulation (e.g. {size: "small"})
-        },
-      },
+      // {
+      //   it: 'default',
+      //   props: {},
+      // },
+    ],
+  },
+  {
+    describe: 'sizes',
+    its: [
+      // {
+      //   it: 'small',
+      //   props: {
+      //     size: 'small'
+      //   },
+      // },
+      // {
+      //   it: 'medium',
+      //   props: {
+      //     size: 'medium'
+      //   },
+      // },
+      // {
+      //   it: 'large - (default)',
+      //   props: {
+      //   },
+      // },
+    ],
+  },
+];
+
+const rtlTests = [
+  {
+    describe: 'rtl',
+    its: [
+      // {
+      //   it: 'rtl',
+      //   props: {
+      //   },
+      // },
     ],
   },
 ];
@@ -29,6 +83,21 @@ tests.forEach(({ describe, its }) => {
       module,
     ).add(it, () => (
       <MarketingPageTestimonialsFooter {...commonProps} {...props} />
+    ));
+  });
+});
+
+rtlTests.forEach(({ describe, its }) => {
+  its.forEach(({ it, props }) => {
+    storiesOf(
+      `${MarketingPageTestimonialsFooter.displayName}${
+        describe ? '/' + describe : ''
+      }`,
+      module,
+    ).add(it, () => (
+      <RTLWrapper rtl>
+        <MarketingPageTestimonialsFooter {...commonProps} {...props} />
+      </RTLWrapper>
     ));
   });
 });
