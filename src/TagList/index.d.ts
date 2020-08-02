5ede6e91e44d828caa7c3a39acad58c03e2ad6a3
Daniel Gendelman <gendeld@gmail.com>
2020-08-02 14:33:12 +0300

    <TagList/> - Use OmitPolyfill and specify that we ignore Tag's onRemove (#5779)
    * Use OmitPolyfill and specify that we ignore Tag's onRemove
    
    * Ban omit using eslint

--- src/TagList/index.d.ts
+++ src/TagList/index.d.ts
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import { TagProps } from '../Tag';
 import { ButtonProps } from '../Button';
+import { OmitPolyfill } from '../common';
 
 export type TagListSize = 'small' | 'medium' | 'large';
 
 export type TagListProps = {
   dataHook?: string;
-  tags: Array<Omit<TagProps, 'size'>>;
+  tags: Array<OmitPolyfill<TagProps, 'size' | 'onRemove'>>;
   size?: TagListSize;
   onTagRemove?: (id: string) => void;
   actionButton?: TagListActionButtonProps;
