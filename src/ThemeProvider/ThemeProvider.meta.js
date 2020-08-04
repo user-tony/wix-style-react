e0de35d61e87bdc117cb85fd5538787ac1d08399
argshook <argshook@gmail.com>
2020-08-04 15:58:56 +0300

    fix(FacesRatingBar ThemeProvider): upgrade .meta.js API

--- src/ThemeProvider/ThemeProvider.meta.js
+++ src/ThemeProvider/ThemeProvider.meta.js
@@ -3,6 +3,6 @@ import Registry from '@ui-autotools/registry';
 
 const metadata = Registry.getComponentMetadata(ThemeProvider);
 
-metadata.exportedFrom({
+metadata.exportInfo = {
   path: 'src/ThemeProvider/ThemeProvider.js',
-});
+};
